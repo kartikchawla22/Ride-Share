2dc3cc03329afc223a9ea03f34420e0ba1292dc4
bharath2059 <mamidibharathreddy2059@gmail.com>
2022-07-26 17:38:25 -0400

    RideList added

--- App.js
+++ App.js
@@ -6,15 +6,12 @@
 
 import 'react-native-gesture-handler';
 import React from 'react';
-import type { Node } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { createStackNavigator } from '@react-navigation/stack';
+import type {Node} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {createStackNavigator} from '@react-navigation/stack';
 
-import {
-  StatusBar,
-  useColorScheme
-} from 'react-native';
+import {StatusBar, useColorScheme} from 'react-native';
 
 import LoginPage from './src/screens/Loginpage';
 import SignupPage from './src/screens/signup-page';
@@ -24,23 +21,28 @@ import SearchRide from './src/screens/searchride';
 import DrawerNavigationDelegate from './src/utils/drawer-navigation-delegate';
 import ShareYourRidePage from './src/screens/share-your-ride-page';
 import profilePage from './src/screens/profilePage';
+import RideList from './src/screens/RideList';
 const Stack = createStackNavigator();
 const App: () => Node = () => {
-
-
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName= "Login" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Login" component={LoginPage} />
+        <Stack.Navigator
+          initialRouteName="Login"
+          screenOptions={{headerShown: false}}>
+          <Stack.Screen name="Login" component={LoginPage} />
           <Stack.Screen name="SignUp" component={SignupPage} />
-          
+
           <Stack.Screen name="Home" component={HomePage} />
           <Stack.Screen name="Confirmation" component={ConfirmationPage} />
           <Stack.Screen name="SearchRide" component={SearchRide} />
-          <Stack.Screen name="DrawerNavigationDelegate" component={DrawerNavigationDelegate} />
+          <Stack.Screen
+            name="DrawerNavigationDelegate"
+            component={DrawerNavigationDelegate}
+          />
           <Stack.Screen name="ShareYourRide" component={ShareYourRidePage} />
           <Stack.Screen name="Profile" component={profilePage} />
+          <Stack.Screen name="RideList" component={RideList} />
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
