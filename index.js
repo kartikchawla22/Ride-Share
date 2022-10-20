6027c506f7baf21bf427b14c72a2a1aa2689d507
bharath2059 <mamidibharathreddy2059@gmail.com>
2022-10-19 21:42:27 -0400

    puash notifications for andriod added

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
 /**
  * @format
  */
-import { Navigation } from "react-native-navigation";
+import {Navigation} from 'react-native-navigation';
+import messaging from '@react-native-firebase/messaging';
+import {AppRegistry} from 'react-native';
 
 import App from './App';
 
 Navigation.registerComponent('Splash', () => App);
 Navigation.events().registerAppLaunchedListener(() => {
-    const options = {
-        topBar: {
-            visible: false,
-            statusbar: { visible: false }
-        }
-    };
-    Navigation.setRoot({
-        root: {
-            stack: {
-                children: [
-                    {
-                        component: {
-                            name: 'Splash'
-                        }
-                    }
-                ],
-                options: options
-            }
-        }
-    });
+  const options = {
+    topBar: {
+      visible: false,
+      statusbar: {visible: false},
+    },
+  };
+  Navigation.setRoot({
+    root: {
+      stack: {
+        children: [
+          {
+            component: {
+              name: 'Splash',
+            },
+          },
+        ],
+        options: options,
+      },
+    },
+  });
 });
+// Register background handler
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  console.log('Message handled in the background!', remoteMessage);
+});
+
+AppRegistry.registerComponent('app', () => App);
