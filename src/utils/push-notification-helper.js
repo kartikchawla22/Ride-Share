642a3116cc6ff724f38f014dd6a0dcfabb8ba6d3
bharath2059 <mamidibharathreddy2059@gmail.com>
2022-10-20 10:36:24 -0400

    Push notification changes

--- src/utils/push-notification-helper.js
+++ src/utils/push-notification-helper.js
@@ -1,6 +1,5 @@
 import messaging from '@react-native-firebase/messaging';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {async} from 'validate.js';
 
 export async function requestUserPermission() {
   const authStatus = await messaging().requestPermission();
